e1df92e6efd06e9fc1e55865b2b2790deead87d9
Anuka <anuuhai2000@gmail.com>
2021-12-20 12:48:59 -0500

    Start building game start mechanics

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ class Game:
     def __init__(self):
         self.deck = buildDeck()
         self.deck.generate()
-        self.player = Player()
+        player = input("What's your name?")
+        self.player = Player(player, True, self.deck)
+        self.computer = Player("Computer", False, self.deck)
+
+    def start(self):
+        p_status = self.player.draw(5)
+        d_status = self.computer.draw(5)
+
+        self.player.show()
+        self.computer.show()
+
+newgame = Game()
+newgame.start
