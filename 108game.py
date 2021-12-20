fbbff125a9d5fdb58a14bdcda8f073c3cce40428
Anuka <anuuhai2000@gmail.com>
2021-12-20 12:50:01 -0500

    Renamed 108game file to main

--- 108game.py
+++
@@ -1,8 +0,0 @@
-from deck import buildDeck
-from player import Player
-
-class Game:
-    def __init__(self):
-        self.deck = buildDeck()
-        self.deck.generate()
-        self.player = Player()
