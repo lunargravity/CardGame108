6adad7c49184390bf268fccafad0acdb551fb772
Anuka <anuuhai2000@gmail.com>
2021-12-29 13:24:12 -0500

    Add action function: draw and play

--- player.py
+++ player.py
@@ -4,14 +4,53 @@ from deck import buildDeck
 from card import Card
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, isPlayer = True):
         self.name = name.title()
+        self.isPlayer = isPlayer
         self.hand = []
+        self.score = 0
 
     def draw(self, deck, times=1):
         for i in range(times):
             self.hand.append(deck.draw())
         return self
+
+    def add_points(self):
+        counter = 0
+        for card in self.hand:
+            self.score += card.points
+        return self.score
+    
+    def check_score(self):
+        print(self.score)
+
+    def clear_hand(self):
+        self.hand = []
+
+    def action(self):
+        action = input("Action: ").upper()
+        for card in self.hand:
+            if card.show() == action:
+                self.hand.remove(card)
+                self.show()
+                break
+            else:
+                if action == "D":
+                    self.draw(deck, 1)
+                    self.show()
+                    break
+                elif action == "P":
+                    #Make it the next player's turn
+                    print("passes to next player")
+                    break
+                elif action == "S":
+                    #Make it show stats
+                    print("stats something")
+                    break
+                else:
+                    print("Invalid action")
+                    self.action()
+                    break
 
     def show(self):
         hand = ""
@@ -25,5 +64,15 @@ deck.shuffle()
 tester = Player("dummy")
 tester.draw(deck, 5)
 tester.show()
+tester.action()
+#tester.add_points()
+#tester.check_score()
+#tester.clear_hand()
+#tester.draw(deck, 2)
+#tester.show()
+#tester.add_points()
+#tester.check_score()
+
+
 
 
