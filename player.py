ae42cf044622234307529cb9847e86d78bde37b5
Anuka <anuuhai2000@gmail.com>
2021-12-29 12:03:24 -0500

    Simplified player class

--- player.py
+++ player.py
@@ -4,36 +4,23 @@ from deck import buildDeck
 from card import Card
 
 class Player:
-    def __init__(self, name = "Player", isPlayer = True, deck = None):
-        player = name.title()
-        self.name = player
+    def __init__(self, name):
+        self.name = name
         self.hand = []
-        self.isPlayer = isPlayer
-        self.deck = deck
-        self.points = 0
 
-    def draw_card(self, times = 1):
-        self.hand.extend(self.deck.draw(times))
-
-    def check_points(self):
-        counter = 0
-        self.points = 0
-        for card in self.hand:
-            self.points += card.points
-        return self.points
-
-    def play_card(self):
-        pass
+    def draw(self, deck, times=1):
+        for i in range(times):
+            self.hand.append(deck.draw())
+        return self
 
     def show(self):
-        hand = ""
-        if self.isPlayer:
-            print("{name}'s cards: ").format(name=self.name)
-            for card in self.hand:
-                hand += "|" + str(display_card(card))
-        else:
-            print("Computer's cards: ")
-            print(str(len(self.hand)) + " cards")
+        for card in self.hand:
+            card.show()
 
+#deck = buildDeck()
+#deck.shuffle()
+#tester = Player("dummy")
+#tester.draw(deck)
+#tester.show()
 
 
