949658f197d713ecf90461810d2da10e5408693c
Anuka <anuuhai2000@gmail.com>
2021-12-30 11:50:12 -0500

    Add cpu subclass to player class and status message

--- player.py
+++ player.py
@@ -4,9 +4,8 @@ from deck import buildDeck
 from card import Card
 
 class Player:
-    def __init__(self, name, isPlayer = True):
+    def __init__(self, name):
         self.name = name.title()
-        self.isPlayer = isPlayer
         self.hand = []
         self.score = 0
 
@@ -22,6 +21,7 @@ class Player:
         return self.score
     
     def check_score(self):
+        return self.score
         print(self.score)
 
     def clear_hand(self):
@@ -44,12 +44,11 @@ class Player:
                     print("passes to next player")
                     break
                 elif action == "S":
-                    #Make it show stats
-                    print("stats something")
+                    status()
                     break
                 else:
                     print("Invalid action")
-                    self.action()
+                    self.action()   
                     break
 
     def show(self):
@@ -59,20 +58,59 @@ class Player:
             hand += "|" + str(card.show()) + "|"
         print(hand)
 
-deck = buildDeck()
-deck.shuffle()
-tester = Player("dummy")
-tester.draw(deck, 5)
-tester.show()
-tester.action()
-#tester.add_points()
-#tester.check_score()
-#tester.clear_hand()
-#tester.draw(deck, 2)
-#tester.show()
-#tester.add_points()
-#tester.check_score()
+class CPU(Player):
+    def __init__(self, name = "CPU"):
+        self.name = name
+        self.hand = []
+        self.score = 0
 
+    def cpu_play(hand, current_card, status):
+        """
+        The logic behind the cpu when playing.
+        """
+        valid = []
+        special = []
 
+        for card in hand:
+            if card.check_face == current_card.check_face:
+                valid.append(card)
+            elif card.check_suit == current_card.check_suit:
+                valid.append(card)
+            elif card.check_face == "Q" or card.check_face == "7" or card.check_face == "A":
+                special.append(card)
 
+        #Refine this later
+        try:
+            choice = random.choice(valid)
+        except:
+            choice = random.choice(special)
 
+        if choice == None:
+            return False
+        else:
+            return choice
+
+    def action(self):
+        try:
+            cpu_play()
+        except:
+            self.draw(deck, 1)
+            try:
+                cpu_play()
+            except:
+                self.next()
+
+    def show(self):
+        print("{name}'s cards: {num}".format(name=self.name, num=int(len(self.hand))))
+
+#Status Message
+def status():
+    s  = ("+---------------------------------------------------------------+\n")
+    s += ("| Stats:\n")
+    s += ("| Scores:\n")
+    s += ("| Your Score: {ysco}\n").format(ysco=user.check_score())
+    s += ("| CPU Score: \n")
+    for i in range(1, int(num_cpu) + 1):
+        s += ("| CPU " + str(i) + ": " + str(CPU("CPU " + str(i)).check_score()))
+    s += ("+---------------------------------------------------------------+\n")
+    print(s)
