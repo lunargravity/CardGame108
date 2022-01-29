566e924363e3ebd8c406dc5484fd231b9031dab3
Anuka <anuuhai2000@gmail.com>
2022-01-29 12:59:59 -0500

    Divide 108 score in half

--- player.py
+++ player.py
@@ -2,7 +2,7 @@
 
 from deck import buildDeck
 from card import Card
-from main import *
+#from main import *
 
 class Player:
     def __init__(self, name, brain = "CPU"):
@@ -17,14 +17,17 @@ class Player:
         return self
 
     def add_points(self):
-        counter = 0
         for card in self.hand:
             self.score += card.points
-        return self.score
+        if self.score == 108:
+            self.score = int(self.score / 2)
+            return self.score
+        else:
+            return self.score
     
     def check_score(self):
-        return self.score
         print(self.score)
+        return self.score
 
     def show(self):
         hand = ""
@@ -36,13 +39,13 @@ class Player:
     def cpu_show(self):
         print("{name}'s cards: {num}".format(name=self.name, num=int(len(self.hand))))
 
-    
-        
 
+#When testing, comment out import main, otherwise it will run the program twice
 
 #deck = buildDeck()
 #deck.shuffle()
-#tester = Player("bob", True)
-#tester.draw(deck, 5)
-#tester.show() 
-#tester.add_points
+#tester = Player("bob", "Human")
+#tester.score = 108
+#print(tester.score)
+#tester.add_points()
+#tester.check_score()
